28b8f049d10191febe7c8a2231ffbc19fa758379
samuelshola777 <samuelshola14@gmail.com>
2022-11-30 23:04:55 +0100

    working with loops

---
+++ src/PlayWithLoop/TwoForLoops.java
@@ -0,0 +1,21 @@
+package PlayWithLoop;
+
+public class TwoForLoops {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("my name is samuel shola");
+            for (int j = 0; j < 10; j++) {
+                System.out.println();
+                System.out.println(j + "             a  me and goat   ");
+                if (i == 9) {
+                    System.out.println();
+                    System.out.println("you're up to 10");
+                }
+            }
+            //   System.out.println(i+"b    ");
+
+
+        }
+    }
+}
